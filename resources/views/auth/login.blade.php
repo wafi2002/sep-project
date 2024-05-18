4c58984b4903c32bfd80e0a818216d81469401c1
wafi2002 <wafi2002ramli@gmail.com>
2024-05-18 15:34:39 +0800

    test commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,7 +62,7 @@
 
         </div>
 
-        {{-- <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
@@ -72,6 +72,6 @@
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
-        </div> --}}
+        </div>
     </form>
 </x-guest-layout>
